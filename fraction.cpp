7512b853d3279fe45da61e426587d82628141d3c
LinFFF5 <2545941332@qq.com>
2025-03-19 19:44:24 +0800

    完善fraction类的函数操作

--- fraction.cpp
+++ fraction.cpp
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "generation.h"
 
 class Fraction {
 private:
@@ -7,29 +7,31 @@ private:
 	int denominator;
 
 public:
-
-	//析构函数
-	Fraction(int integerpart, int num, int den) {
+	//默认构造函数
+	Fraction(int num = 0, int den = 1) : integer(0), numerator(num), denominator(den) {
+		if (denominator == 0) {
+			std::cerr << "Error: Denominator cannot be zero!" << std::endl;
+			denominator = 1;
+		}
+		simplify();
+	}
+	//构造函数
+	Fraction(int integerpart, int num, int den) : integer(integerpart), numerator(num), denominator(den){
 		if (den == 0) {
 			throw std::invalid_argument("Denominator cannot be zero.");
 		}
-		integer = integerpart;
-		numerator = num;
-		denominator = den;
 		simplify();
 	}
 
 	//化简
 	void simplify() {
 
-		if (integer != 0) {
+		if (numerator >= denominator) {
+
 			integer += numerator / denominator;
+			numerator = numerator % denominator;
 		}
-		else {
-			integer = numerator / denominator;
-		}
-		//更新分子
-		numerator = numerator % denominator;
+
 		// 使用最大公约数简化分数
 		int gcd_value = gcd(abs(numerator), denominator); 
 		numerator /= gcd_value;
@@ -44,21 +46,27 @@ public:
 		int new_denominator = denominator * other.denominator;
 		return Fraction(0, new_numerator, new_denominator);
 	}
+
 	//减法
 	Fraction operator - (const Fraction& other) const {
+
 		int new_numerator = (integer * denominator + numerator) * other.denominator
 			- (other.integer * other.denominator + other.numerator) * denominator;
 		int new_denominator = denominator * other.denominator;
 		return Fraction(0, new_numerator, new_denominator);
 	}
+
 	//乘法
 	Fraction operator * (const Fraction& other) const {
+
 		int new_numerator = (integer * denominator + numerator) * (other.integer * other.denominator + other.numerator);
 		int new_denominator = denominator * other.denominator;
 		return Fraction(0, new_numerator, new_denominator);
 	}
+
 	//除法
 	Fraction operator/(const Fraction& other) const {
+
 		if (other.numerator == 0) {
 			throw std::invalid_argument("Cannot divide by a fraction with a numerator of zero.");
 		}
@@ -67,4 +75,21 @@ public:
 		return Fraction(0, new_numerator, new_denominator);
 	}
 
+	string toString() const {
+
+		stringstream ss;
+		if (integer != 0) {
+			ss << integer;
+			if (numerator != 0) {
+				ss << "'" << numerator << "/" << denominator;
+			}
+		}
+		else if (numerator != 0) {
+			ss << numerator << "/" << denominator;
+		}
+		else {
+			ss << "0";
+		}
+		return ss.str();
+	}
 };
